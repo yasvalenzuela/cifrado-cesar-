d043e6e87a230a59289923cf71678b8078d43162
yasvalenzuela <yasna.valenzuela91@gmail.com>
2018-01-25 23:22:09 -0300

    actualizando segun guia de estilos de google

--- app.js
+++ app.js
@@ -1,37 +1,32 @@
-//var sentence = prompt("Escribe una frase"); //con esto pido al usuario que escriba una frase
-function answer (){ //creo una funcion 
-	do{
-		var answerFirst = prompt("Indique la opción que desea realizar: 1) Cifrar - 2) Decifrar"); //le pido al usuario lo que quiere hacer
+// var sentence = prompt("Escribe una frase"); // con esto pido al usuario que escriba una frase
+function answer() { // creo una funcion 
+  do {
+    var answerFirst = prompt('Indique la opción que desea realizar: 1) Cifrar - 2) Decifrar'); // le pido al usuario lo que quiere hacer
 
-		if(answerFirst !== ""){ //si es distinto a string vacio
-            if(answerFirst === "1"){ //si es igual a 1
-                cipher(); //llamo a cipher
-                }else if (answerFirst == "2"){ //sino se cumplen las condiciones anteriores y la opcion es 2
-			    decipher(); //llamo a decipher
-				}else { //sino mandara la alerta
-					alert("Indique una opción válida");
-				}
-			}
-	
-	}while (answerFirst === "" || (answerFirst!== "1" && answerFirst !== "2")); //para que siga pidiendo que ingrese una opcion hasta que sea correcta
-			
-
+    if (answerFirst !== '') { // si es distinto a string vacio
+      if (answerFirst === '1') { // si es igual a 1
+        cipher(); // llamo a cipher
+      } else if (answerFirst === '2') { // sino se cumplen las condiciones anteriores y la opcion es 2
+			    decipher(); // llamo a decipher
+      } else { // sino mandara la alerta
+        alert('Indique una opción válida');
+      }
+    }
+  } while (answerFirst === '' || (answerFirst !== '1' && answerFirst !== '2')); // para que siga pidiendo que ingrese una opcion hasta que sea correcta
 }
 
-function cipher(){ //creo la funcion con la cual hare el cifrado
-	var sentence = prompt("Escriba una palabra por favor"); //le pido al usuario que ingrese una frase la cual cifrare
-    var cont = ""; //variable vacia aqui quedara los nuevo valores
-    for(var i=0; i < sentence.length; i++){ //para que recorra el string
-    	var letterFirst = sentence.charCodeAt(i);// tengo el codigo ASCII
-    		letterFirst = (((((letterFirst - 65) + 33) + 26) % 26 ) + 65); //formula de cifrado
-    		letterFirst = String.fromCharCode(letterFirst); //con esto lo cifrado lo paso a letra
-    		cont = cont + letterFirst; //resultado para cada indice se va a guardar en la variable cont
-    	}
-    	return alert("Así queda tu texto cifrado: \n" + cont); //alerta que retorna 
-    
-	}
-	
-	/*
+function cipher() { // creo la funcion con la cual hare el cifrado
+  var sentence = prompt('Escriba una palabra por favor'); // le pido al usuario que ingrese una frase la cual cifrare
+  var cont = ''; // variable vacia aqui quedara los nuevo valores
+  for (var i = 0; i < sentence.length; i++) { // para que recorra el string
+    var letterFirst = sentence.charCodeAt(i); // tengo el codigo ASCII
+    letterFirst = (((((letterFirst - 65) + 33) + 26) % 26) + 65); // formula de cifrado
+    letterFirst = String.fromCharCode(letterFirst); // con esto lo cifrado lo paso a letra
+    cont = cont + letterFirst; // resultado para cada indice se va a guardar en la variable cont
+  }
+  return alert('Así queda tu texto cifrado: \n' + cont); // alerta que retorna 
+}
+/*
 	var resultado = ""; //crear string vacío para ir concatenándole los índices cifrados
 	for (var i = 0 ; i < str.length ; i++){ 
 		if (str[i] == " "){ //si el índice es un espacio, devolver un espacio
@@ -53,38 +48,20 @@ function cipher(){ //creo la funcion con la cual hare el cifrado
 
     	}
 */
-    	
 
-    
+function decipher() {
+  var sentence = prompt('Escriba una frase por favor');
+  var count = '';
 
-function decipher(){
-	
-	var sentence = prompt("Escriba una frase por favor");
-	var count ="";
-
-    for(var i=0; i < sentence.length; i++){
-    	var letterSecond = sentence.charCodeAt(i);//codigo ASCII
-    		letterSecond = ((((letterSecond - 65) + 33) + 26) % 26 ) + 65;
-    		letterSecond = String.fromCharCode(letterSecond);
-    		count = count + letterSecond;
-    	}
-    	return alert("Así queda tu texto decifrado: \n" + count);
-
-
+  for (var i = 0; i < sentence.length; i++) {
+    var letterSecond = sentence.charCodeAt(i); // codigo ASCII
+    letterSecond = ((((letterSecond - 65) + 33) + 26) % 26) + 65;
+    letterSecond = String.fromCharCode(letterSecond);
+    count = count + letterSecond;
+  }
+  return alert('Así queda tu texto decifrado: \n' + count);
 }
-
-
-
-
-	
-
-	
-answer(); 	
-
-
-
-
-
+answer();
 
 /*
 function convertDoubleSpaceToSingle(str) {
